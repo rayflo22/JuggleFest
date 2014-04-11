e2258d217589a646f9f7921259a2457fd1980e5d
rayflo22 <ray.flo22@yahoo.com>
2014-04-11 16:41:06 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  JuggleFest1
+//
+//  Created by Raymond Flores on 3/29/14.
+//  Copyright (c) 2014 Raymond Flores. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+#include "juggleFest.h"
+
+int main()
+{
+    JuggleFest juggleFest("jugglefest.txt");
+    return 0;
+}
